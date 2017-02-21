7378c1c936b6321517b64ba645439e4c699921dc
Raul <raullarosa.personal@gmail.com>
2017-02-21 15:53:49 -0500

    Imported NRFManager wrapper file and arduino file

---
+++ iOS/EpiLife/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  EpiLife
+//
+//  Created by Raul La Rosa on 2/16/17.
+//  Copyright © 2017 EpiLife. All rights reserved.
+//
+
+import UIKit
+
+private class ViewController: UIViewController, NRFManager {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+
+}
+
