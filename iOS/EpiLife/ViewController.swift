d49a01e3a4969ce3b167f5187f0e3978e5cafca2
Raul <raullarosa.personal@gmail.com>
2017-02-21 20:48:55 -0500

    beginning to implement connect/disconnect functions

--- iOS/EpiLife/ViewController.swift
+++ iOS/EpiLife/ViewController.swift
@@ -8,14 +8,43 @@
 
 import UIKit
 
-private class ViewController: UIViewController {
+private class ViewController: UIViewController, NRFManagerDelegate {
 
+    var nrfManager:NRFManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nrfManager = NRFManager(delegate:self)
     }
     
+    func sendData(){
+        let result = self.nrfManager.writeString("Data Sent: Hello World!")
+    }
 
-
+    
+    // NRFManagaerDelgate methods
+    func nrfDidConnect(_ nrfManager: NRFManager) {
+        print("D: ★ Connected")
+        self.sendData()
+    }
+    
+    func nrfDidDisconnect(_ nrfManager: NRFManager) {
+        print("D: ★ Connected")
+    }
+    
+    func nrfReceivedData(_ nrfManager: NRFManager, data: Data?, string: String?) {
+        print("D: ⬇ Received data - String: \(string) - Data: \(data)")
+    }
+    
+    
+    @IBAction func connectBtn(_ sender: UIButton) {
+        
+        print("Connect pressed")
+    }
+    
+    @IBAction func disconnectBtn(_ sender: UIButton) {
+        
+        print("Disconnect pressed")
+    }
 }
 
